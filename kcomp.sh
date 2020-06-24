b568ee0d7d9d4beaf39b1bb4a2eabff86354a858
ThatGeekyWeeb <ssfgames13@gmail.com>
2020-06-24 12:33:42 -0400

    first commit

---
+++ kcomp.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+grep "OpenGLIsUnsafe=true" "$HOME/thatgeekyweeb/.config/kwinrc" || exit 1
+IFS=""
+var1=$(sed '1,/Windows/d' "$HOME/.config/kwinrc")
+var2=$(sed '/Comp/Q' "$HOME/.config/kwinrc")
+# var2 = `update_info=kwin.upd:replace-scalein-with-scale,kwin.upd:port-minimizeanimation-effect-to-js,kwin.upd:port-scale-effect-to-js,kwin.upd:port-dimscreen-effect-to-js,kwin.upd:auto-bordersize,kwin.upd:animation-speed`
+printf "$var2"'%b\n'"$var1" > "$HOME/.config/kwinrc"
+printf '%b\n[Compositing]%b\nGLCore=true%b\nOpenGLIsUnsafe=false%b\nWindowsBlockCompositing=false' >> "$HOME/.config/kwinrc"
+nohup kwin_x11 --replace &
+exit 0
